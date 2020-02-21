9fb7042b242eec435809d53437d58d95f8af93d4
jinalshah999 <jinalshah999@gmail.com>
2020-02-21 18:29:58 +0530

    added demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewContainerRef,
+  ComponentFactoryResolver
+} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'demoLazyComponent';
+  title = "demoLazyComponent";
+  constructor(
+    private viewcontref: ViewContainerRef,
+    private compfactresol: ComponentFactoryResolver
+  ) {}
+  loadComponent() {
+    import('./demo1/demo1.component').then(({Demo1Component})=>{
+      this.viewcontref.clear();
+      const cmp=this.compfactresol.resolveComponentFactory(Demo1Component);
+      const cmpref=this.viewcontref.createComponent(cmp);
+      cmpref.instance.data="Hello World";
+    });
+  }
 }
